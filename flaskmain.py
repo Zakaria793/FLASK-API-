fee0413807f561d8e1bf0b755c7493185e621426
Z.B <74127312+Zakaria793@users.noreply.github.com>
2022-04-12 17:16:46 +1000

    Add files via upload
    flask files for planet data

---
+++ flaskmain.py
@@ -0,0 +1,12 @@
+from flask import Flask,jsonify
+from flaskdata import data
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return jsonify({
+        "data":data,
+        "message": "success"
+    }) ,200
+    
+if __name__=="__main__":
+    app.run()
